095d86b89721fd1d6001948ccef3d1a056e76cd7
NumericFactory <frederic.lossignol@gmail.com>
2020-10-01 15:39:15 +0200

    5 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ApiComponent } from './api/api.component';
-import { ContactlistComponent } from './contactlist/contactlist.component';
+import { ContactlistComponent } from './contact-container/contactlist/contactlist.component';
 
 
 const routes: Routes = [
--- src/app/contact-container/contactlist/contactlist.component.ts
+++ src/app/contact-container/contactlist/contactlist.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../models/user.interface';
-import { UserService } from '../services/user.service';
+import { User } from '../../models/user.interface';
+import { UserService } from '../../services/user.service';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
