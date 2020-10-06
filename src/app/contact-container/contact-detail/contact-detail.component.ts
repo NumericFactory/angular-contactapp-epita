528a90fde76f1a8cb9caa9221833cdef9b93396d
NumericFactory <frederic.lossignol@gmail.com>
2020-10-06 14:14:04 +0200

    2 files modified

--- src/app/contact-container/contact-detail/contact-detail.component.ts
+++ src/app/contact-container/contact-detail/contact-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { User } from '../../models/user.interface';
 import { UserService } from '../../services/user.service';
 
 @Component({
@@ -10,6 +11,7 @@ import { UserService } from '../../services/user.service';
 })
 export class ContactDetailComponent implements OnInit {
   userId:number;
+  user:User;
   userRessources$:Observable<any>;
   todos:Array<any>;
   dones:Array<any>;
@@ -17,14 +19,18 @@ export class ContactDetailComponent implements OnInit {
   constructor(private route: ActivatedRoute, private userService:UserService) { }
 
   ngOnInit() {
+    
     this.userRessources$ = this.userService.userRessources$
     
-    // this.route.snapshot.params
+    // this.route.snapshot.params.id
     // permet de récupérer les paramètres de l'url de manière statique
 
     // this.route.params.subscribe()
     // permet de récupérer les paramètres de l'url de manière dynamique
     this.route.params.subscribe(routeParams => {
+      // Charger le Contact
+      this.user = this.userService.getUsersValue().find(user => routeParams.id == user.id)
+
       // Charger les posts, comments et todos du user
       this.userService.loadUserRessources(routeParams.id);
       this.userRessources$.subscribe(ressources => {
