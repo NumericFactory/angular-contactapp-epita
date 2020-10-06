16aebcc1016021354941ecdda4cf6441376a6953
NumericFactory <frederic.lossignol@gmail.com>
2020-10-06 03:20:54 +0200

    2 files modified

--- src/app/contact-container/contact-detail/contact-detail.component.ts
+++ src/app/contact-container/contact-detail/contact-detail.component.ts
@@ -11,24 +11,29 @@ import { UserService } from '../../services/user.service';
 export class ContactDetailComponent implements OnInit {
   userId:number;
   userRessources$:Observable<any>;
+  todos:Array<any>;
+  dones:Array<any>;
 
   constructor(private route: ActivatedRoute, private userService:UserService) { }
 
   ngOnInit() {
     this.userRessources$ = this.userService.userRessources$
     
-    //this.userId = this.route.snapshot.params.id; // ex:{id:109}
+    // this.route.snapshot.params
+    // permet de récupérer les paramètres de l'url de manière statique
+
+    // this.route.params.subscribe()
+    // permet de récupérer les paramètres de l'url de manière dynamique
     this.route.params.subscribe(routeParams => {
       // Charger les posts, comments et todos du user
       this.userService.loadUserRessources(routeParams.id);
-      // S'abonner à l'Observable
+      this.userRessources$.subscribe(ressources => {
+        this.todos = ressources.todos.filter( todo => todo.completed==false)
+        this.dones = ressources.todos.filter( todo => todo.completed==true)
+      })
      
-    });
-    
-   
-  
-    
+    });    
 
-  }
+  } // fin ngOnInit
 
 }
